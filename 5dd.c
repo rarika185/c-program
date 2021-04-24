45bb1a47113ecd43bf6484e932748d10ecab5f45
rarika185 <83079535+rarika185@users.noreply.github.com>
2021-04-24 10:40:53 +0530

    Add files via upload

---
+++ 5dd.c
@@ -0,0 +1,25 @@
+#include<stdio.h>
+void main()
+{
+	int n,s=0,d,i;
+	printf("enter the number");
+	scanf("%d",&n);
+	if(n>=10000 && n<=99999)
+	{
+		while(n>0)
+		{
+			d=n%10;
+			s=s+d;
+			n=n/10;
+			i++;
+		
+		
+		}
+		printf("sum is =%d",s);
+	}
+	else
+	{
+		printf("invalid number\n");
+	}	
+	
+}
