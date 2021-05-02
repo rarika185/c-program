576c45b6cbb44cac2b434af441fb4cac25814e51
rarika185 <83079535+rarika185@users.noreply.github.com>
2021-05-02 17:43:48 +0530

    Add files via upload

---
+++ even.c
@@ -0,0 +1,11 @@
+#include<stdio.h>
+void main()
+{
+	int a;
+	printf("enter the number");
+	scanf("%d",&a);
+	if(a%2==0)
+	{
+		printf("%d is even",a);
+	}
+}
---
+++ sum.c
@@ -0,0 +1,11 @@
+#include<stdio.h>
+void main()
+{
+	int a,b,s;
+	printf("enter the first number");
+	scanf("%d",&a);
+	printf("enter the second number");
+	scanf("%d",&b);
+	s=a+b;
+	printf("%d",s);
+}
